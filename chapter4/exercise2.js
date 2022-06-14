7accdd01997fb65b0ed8d5fd8871916e069af1a5
teixeirajr <teixeirajunior1710@gmail.com>
2022-06-13 23:30:30 -0300

    exercise 2 chapter 4 rebuild

--- chapter4/exercise2.js
+++ chapter4/exercise2.js
@@ -1,8 +1,7 @@
 /*
 Recursão
-Nós vimos que o % (operador resto) pode ser usado para testar se um número é par ou ímpar,
- usando % 2 para verificar se ele é divisível por dois. 
- Abaixo, está uma outra maneira de definir se um número inteiro positivo é par ou ímpar:
+Defina uma função recursiva isEven que satisfaça as condições descritas acima. 
+A função deve aceitar um número como parâmetro e retornar um valor Booleano.
 */
 
 function isEven(number) {
